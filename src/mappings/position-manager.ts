a76d36783032fc791a1a363bd5b50ec1dfb2b213
Croooook <rajatgoyal6404@gmail.com>
2022-02-21 16:44:24 +0530

    transferred the code from entities to mappings

--- src/mappings/position-manager.ts
+++ src/mappings/position-manager.ts
@@ -1,11 +1,60 @@
-import { log } from "@graphprotocol/graph-ts";
+import { log,BigInt } from "@graphprotocol/graph-ts";
 import {
   Collect,
   DecreaseLiquidity,
   IncreaseLiquidity,
   Transfer,
 } from "../../generated/NonfungiblePositionManager/NonfungiblePositionManager";
-import { createOrLoadPosition } from "../entities";
+import {
+  ADDRESS_ZERO,
+  BIG_INT_ZERO,
+  UNISWAP_V3_FACTORY_ADDRESS,
+  UNISWAP_V3_POSITION_MANAGER_ADDRESS,
+} from "./const";
+
+import { Position } from "../../generated/schema";
+import { NonfungiblePositionManager } from "../../generated/NonfungiblePositionManager/NonfungiblePositionManager";
+import { Factory as FactoryContract } from "../../generated/Factory/Factory";
+
+
+export function createOrLoadPosition(tokenId: BigInt): Position | null {
+  let contract = NonfungiblePositionManager.bind(
+    UNISWAP_V3_POSITION_MANAGER_ADDRESS
+  );
+  let positionCall = contract.try_positions(tokenId);
+  if (positionCall.reverted) {
+    // the call reverts in situations where the position is minted
+    // and deleted in the same block
+    return null;
+  }
+
+  let positionResult = positionCall.value;
+  let token0 = positionResult.value2;
+  let token1 = positionResult.value3;
+
+  
+
+  let position = Position.load(tokenId.toString());
+  if (position == null) {
+    let factoryContract = FactoryContract.bind(UNISWAP_V3_FACTORY_ADDRESS);
+    let fee = positionResult.value4;
+    let poolAddress = factoryContract.getPool(token0, token1, fee);
+
+    position = new Position(tokenId.toString());
+    position.owner = ADDRESS_ZERO;
+    position.pool = poolAddress.toHexString();
+    position.tickLower = BigInt.fromI32(positionResult.value5);
+    position.tickUpper = BigInt.fromI32(positionResult.value6);
+    position.liquidity = BIG_INT_ZERO;
+    
+    position.save();
+  } 
+
+  return position;
+}
+
+
+
 
 export function handleIncreaseLiquidity(event: IncreaseLiquidity): void {
   let position = createOrLoadPosition(event.params.tokenId);
@@ -43,8 +92,14 @@ export function handleDecreaseLiquidity(event: DecreaseLiquidity): void {
 
 export function handleCollect(event: Collect): void {
   
-  createOrLoadPosition(event.params.tokenId);
-  
+  let position = createOrLoadPosition(event.params.tokenId);
+
+  // position was not able to be fetched or is not supported
+  if (position == null) {
+    return;
+  }
+  position.save();
+
   log.info(
     "[NonfungiblePositionManager] Collect position={} amount0={} amount1={}",
     [
